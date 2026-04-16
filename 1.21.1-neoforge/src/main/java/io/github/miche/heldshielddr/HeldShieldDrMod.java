710ee6e804245823c7f3f16a9611fb4faa3c52e9
MultiFlame11 <MultiFlame11@users.noreply.github.com>
2026-04-15 22:07:11 -0400

    Bump version to 1.1.2 for durability drain addition

--- 1.21.1-neoforge/src/main/java/io/github/miche/heldshielddr/HeldShieldDrMod.java
+++ 1.21.1-neoforge/src/main/java/io/github/miche/heldshielddr/HeldShieldDrMod.java
@@ -12,7 +12,7 @@ import org.apache.logging.log4j.Logger;
 public class HeldShieldDrMod {
     public static final String MODID = "heldshielddr";
     public static final String NAME = "Held Shield DR";
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.1.2";
     public static final Logger LOGGER = LogManager.getLogger(NAME);
 
     public HeldShieldDrMod(IEventBus modBus, ModContainer container) {
